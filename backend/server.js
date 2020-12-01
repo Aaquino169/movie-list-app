8c0550aa15b548445e4ab1cae0c9eceef38b63c7
alexander_aquino <nanito169@gmail.com>
2020-11-30 19:54:48 -0500

    connected mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 const app = express()
 const port = 8000
 
+app.use(bodyParser.json());
 
-
+//middleware for mongoose
+mongoose.connect("mongodb://localhost:27017/movieListApp", { useNewUrlParser:true })
+mongoose.connection.once('open', () => {
+    console.log('connected to db')
+})
+mongoose.connection.on('error', err =>console.log(err.message))
+mongoose.connection.on('disconnected', () =>console.log('mongo disconnected'))
 
 
 
