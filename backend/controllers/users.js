d4f9cbda8b98a2887f5e93b3ef7fae03a6bd437a
alexander_aquino <nanito169@gmail.com>
2020-12-06 13:06:28 -0500

    created route to add to user list

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt")
 const express = require("express")
 const users = express.Router()
+const fetch =require("node-fetch")
 const Users = require("../models/users")
 
 const isAuthenticated = (req, res, next) =>  {
@@ -22,6 +23,35 @@ users.post('/new', (req,res) => {
         console.log('User Created:', createdUser )
         res.status(200).send(createdUser)
     })
+})
+
+//add to list
+users.put('/addToList/:imdbID', isAuthenticated, async (req,res) => {
+    try {
+        const url = "http://www.omdbapi.com/?"
+        const apiKey = "&apikey=111970bd"
+        const imdbID = "&i="+ req.params.imdbID
+        const type = "&type=movie"
+        const response = await fetch(url+apiKey+imdbID+type);
+        const data = await response.json()
+
+
+        let currentUser = req.session.currentUser
+
+        let userData = await Users.findById(currentUser._id)
+
+        userData["Movie List"].push(data)
+
+        console.log(userData)
+
+        userData.save()
+
+        res.status(200).send(userData)
+
+    } catch (err) {
+
+        res.status(400).json({ err: err.message})
+    }
 })
 
 module.exports = users
